c83d6a8305dc31ae32e4f063bf09afd16790ddbb
Loki Laufeyson <manwiththegoldengun837@gmail.com>
2022-03-30 10:53:33 +0530

    Update graph_plotter.py

--- graph_plotter.py
+++ graph_plotter.py
@@ -1,4 +1,4 @@
-print("""Refer 'Readme' file for solutions of some common issues
+print("""Refer 'Readme' file for solutions of common issues
 You are free to contact me :
   Discord:Loki_Laufeyson#2473 
   Telegram:https://t.me/Loki_Laufeyson_2473 
